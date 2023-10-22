b520a74a1086da0e811b63df4a6b8a234dc0a5ba
David Méndez <davidemv1622@gmail.com>
2023-10-22 18:17:37 -0500

    Agregado mi primer SplashScreen

--- lib/src/splashScreen/splash_1_prueba.dart
+++ lib/src/splashScreen/splash_1_prueba.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'home.dart';
 
@@ -10,87 +11,192 @@ class SplashScreenT extends StatefulWidget {
 }
 
 class _SplashScreenTState extends State<SplashScreenT> with TickerProviderStateMixin {
-  late final AnimationController _controller;
+  late final AnimationController _controller; // Controlador general de animacion
 
-  late Animation<double> controllerAgrandar;
-  late Animation<double> controllerReducir;
+  // Variables para controlar los aspectos especificos e intervalos de cada animacion
+  late Animation<double>? controllerSizeCircularBlue; // Agrandar y reducir circulo azul
+  late Animation<double>? controllerOpacityCircularBlue; // Opacidad al circulo azul al reducirse
+  late Animation<double>? controllerSizeCircularYellow; // Agrandar y reducir circulo amarillo
+  late Animation<double>? controllerOpacityLogo; // Opacidad del logo al inicio del splash
+  late Animation<double>? controllerOpacityIconAndSplashLogo; // Opacidad del icono y del logo ya finalizando el splash
 
+  // Realiza liberacion de recursos para no tener problemas de renddimiento 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
 
+  // Inicia una vez que se muestre la pantalla
   @override
   void initState() {
     super.initState();
-    var timeSplash = Duration(seconds: 2);
+    // **** Se define el tiempo a esperar para pasar a la siguiente pantalla **** 
+    var timeSplash = Duration(seconds: 3);
     Future.delayed(timeSplash, () {
       Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
     });
 
+    // Permite definir el tiempo que va a durar la animación
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 2), // Ajusta la duración total de la animación
+      duration: const Duration(seconds: 1)
     );
+    //..forward(); // "..forward()" permite hacer que se inicie la animación
+    
+    startAnimation();
+  }
 
-    controllerAgrandar = Tween(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(parent: _controller, curve: Interval(0.0, 0.5)),
-    );
+  // Metodo para controlar el inicio, final y el intervalo de cada animacion al iniciarse
+  void startAnimation() {
 
-    controllerReducir = Tween(begin: 1.0, end: 0.0).animate(
-      CurvedAnimation(parent: _controller, curve: Interval(0.5, 1.0)),
-    );
+    if (_controller.status != AnimationStatus.completed) {
 
-    _controller.forward();
-    _controller.addListener(() {
-      setState(() {});
-      print(_controller.status);
-      if (_controller.status == AnimationStatus.completed) {
-        _controller.reverse();
-      }
-    });
+      controllerOpacityLogo = Tween(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.5, 1.0)));
+
+      controllerSizeCircularBlue = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.5, 1.0))..addStatusListener(listenStatus));
+      
+      controllerOpacityCircularBlue = Tween(begin: 1.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.0, 0.8)));
+
+      controllerSizeCircularYellow = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.5, 1.0)));
+
+      controllerOpacityIconAndSplashLogo = Tween(begin: 0.0, end: 0.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.0, 1.0)));
+      
+    } else {
+      controllerOpacityLogo = Tween(begin: 0.0, end: 0.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.0, 1.0)));
+
+      controllerSizeCircularBlue = Tween(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.0, 0.8, curve: Curves.bounceOut)));
+
+      controllerOpacityCircularBlue = Tween(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.0, 0.8)));
+
+      controllerSizeCircularYellow = Tween(begin: 0.0, end: 0.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.0, 1.0,)));
+
+      controllerOpacityIconAndSplashLogo = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Interval(0.2, 1.0)));
+    }
+
+    _controller.reset(); // Reinicia el la animacion (begin: 0.0)
+    _controller.forward(); // Inicio de la animacion
+  }
+
+  void listenStatus(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      controllerSizeCircularBlue!.removeStatusListener(listenStatus);
+      startAnimation();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          AnimatedBuilder(
-            animation: _controller,
-            builder: (BuildContext context, Widget? child) {
-              // Usa el valor de controllerAgrandar para escalar el círculo
-              double scale = controllerAgrandar.value;
+      body: Stack(children: [
 
-              if (_controller.status == AnimationStatus.completed) {
-                // Si la animación está en reversa, usa la animación de reducción
-                scale = controllerReducir.value;
-              }
-
-              return Transform.scale(
-                scale: scale * (MediaQuery.of(context).size.height * 0.03),
-                child: child,
-              );
-            },
+        // Animacion para el icono del final (opacidad)
+        AnimatedBuilder(animation: _controller,
+          builder: (BuildContext context, Widget? child) {
+            return Opacity(
+              opacity: controllerOpacityIconAndSplashLogo!.value,
+              child: child,
+          );},
+          child: const Padding(
+            padding: EdgeInsets.only(top: 100,),
             child: Align(
-              alignment: Alignment.center,
-              child: Container(
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.transparent,
-                  border: Border.all(
-                    color: Colors.amber,
-                    width: 20.0,
-                  ),
-                ),
-              ),
+              alignment: Alignment.topCenter,
+              child: Image(image: AssetImage("assets/icon.png"), height: 200,)
             ),
           ),
-        ],
-      ),
+        ),
+
+        // Animacion del logo del splash del final (Opacidad)
+        AnimatedBuilder(animation: _controller,
+          builder: (BuildContext context, Widget? child) {
+            return Opacity(
+              opacity: controllerOpacityIconAndSplashLogo!.value,
+              child: child,
+          );},
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: SvgPicture.asset( 
+              'assets/logo_splash.svg',
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+        ),
+
+        // Animacion para el circulo amarillo del inicio (Agrandar)
+        AnimatedBuilder(animation: _controller,
+          builder: (BuildContext context, Widget? child) {
+
+          return Transform.scale(
+            scale: controllerSizeCircularYellow!.value * (MediaQuery.of(context).size.height * 0.03),
+            //scale: controllerAgrandar!.value, 
+              child: child,
+          );},
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: 120,
+              height: 120,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.amber,
+                  width: 25.0,
+                ),
+              )
+            ),
+          ),
+        ),
+
+        // Animacion para el circulo azul en la mitad (Agrandar y reduccion con rebote "opacidad en reduccion")
+        AnimatedBuilder(animation: _controller,
+          builder: (BuildContext context, Widget? child) {
+
+          return Opacity(
+            opacity: controllerOpacityCircularBlue!.value,
+            child: Transform.scale(
+              scale: controllerSizeCircularBlue!.value * (MediaQuery.of(context).size.height * 0.03),
+              child: child,
+            ),
+          );},
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              width: 45,
+              height: 45,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+              )
+            ),
+          ),
+        ),
+
+        // Animacion para el logo del inicio (opacidad)
+        AnimatedBuilder(animation: _controller, 
+        builder: (BuildContext context, Widget? child) {
+          return Opacity(opacity: controllerOpacityLogo!.value,
+            child: Align(
+              alignment: Alignment.center,
+              child: SvgPicture.asset( 
+                'assets/logo_credibanco_login.svg',
+                width: 250,
+              ),
+            ),
+          );
+        }),
+
+      ]),
     );
   }
 }
